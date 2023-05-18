1e2b61cd9c0ff4912300eec133d9f17a26b4d4fb
Bogumił <hello@milewski.me>
2023-05-18 14:35:50 +0200

    init strapi project

---
+++ src/api/curiosity/controllers/curiosity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * curiosity controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::curiosity.curiosity');
---
+++ src/api/curiosity/routes/curiosity.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * curiosity router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::curiosity.curiosity');
